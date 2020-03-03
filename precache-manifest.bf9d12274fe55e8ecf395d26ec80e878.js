ecceff14c59262e9629dbd603b45831448526130
Jasan Hernandez <jasan_azael@hotmail.es>
2020-03-03 12:38:16 -0600

    Updates

---
+++ precache-manifest.bf9d12274fe55e8ecf395d26ec80e878.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4834a290d3304a029e1be2ea6392f67a",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "1217364166c5ddd49bc9",
+    "url": "/reactjs/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "15bb5986cbd5bb26507e",
+    "url": "/reactjs/static/css/main.c896e228.chunk.css"
+  },
+  {
+    "revision": "1217364166c5ddd49bc9",
+    "url": "/reactjs/static/js/2.0b29d466.chunk.js"
+  },
+  {
+    "revision": "280b48dbc3281f342da16f66e41cf46c",
+    "url": "/reactjs/static/js/2.0b29d466.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15bb5986cbd5bb26507e",
+    "url": "/reactjs/static/js/main.72eef814.chunk.js"
+  },
+  {
+    "revision": "a7854a8d3f88c6cec79b",
+    "url": "/reactjs/static/js/runtime-main.fbd115ba.js"
+  },
+  {
+    "revision": "cf1c0d9d3b694e2e86d600e365fee83b",
+    "url": "/reactjs/static/media/astronaut.cf1c0d9d.png"
+  },
+  {
+    "revision": "0d2ff286deb3895d3ee82fed78c4dce4",
+    "url": "/reactjs/static/media/astronauts.0d2ff286.svg"
+  },
+  {
+    "revision": "ed7c58eafe17eff3b2557c0369b20264",
+    "url": "/reactjs/static/media/badge-header.ed7c58ea.svg"
+  },
+  {
+    "revision": "6841730f1bbe3235e7a0cdc7d244d244",
+    "url": "/reactjs/static/media/logo.6841730f.svg"
+  },
+  {
+    "revision": "a1880fbfb5634577b88090c9f82b3de7",
+    "url": "/reactjs/static/media/platziconf-logo.a1880fbf.svg"
+  },
+  {
+    "revision": "0abcdfcd1e323649a0338a204fe8b853",
+    "url": "/reactjs/static/media/stars.0abcdfcd.svg"
+  }
+]);
